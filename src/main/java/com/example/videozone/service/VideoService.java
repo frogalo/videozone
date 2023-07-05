d851f3f1acde6711ff140a0abf5a3e07590fed8a
Jakub Urbanski <s23525@pjwstk.edu.pl>
2023-07-05 21:37:28 +0200

    working video uploading

--- src/main/java/com/example/videozone/service/VideoService.java
+++ src/main/java/com/example/videozone/service/VideoService.java
@@ -4,22 +4,28 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @Service
 public class VideoService {
+
     @Autowired
     private GridFsTemplate gridFsTemplate;
 
+    @Transactional
     public void uploadVideo(MultipartFile file) {
         try {
             ObjectId fileId = gridFsTemplate.store(file.getInputStream(), file.getOriginalFilename());
             // Możesz wykonać dodatkowe operacje, np. zapisanie metadanych pliku w kolekcji "metadata"
         } catch (IOException e) {
-            // Obsługa błędu
+            // Obsługa błędu wejścia/wyjścia
+            throw new RuntimeException("Wystąpił błąd podczas przesyłania pliku.", e);
+        } catch (Exception e) {
+            // Obsługa innych wyjątków
+            throw new RuntimeException("Wystąpił nieznany błąd.", e);
         }
     }
 }
-
