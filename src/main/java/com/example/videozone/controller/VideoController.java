da88b24380c55ec2e3b4dae9a0e1059a15c8abd2
Jakub Urbanski <s23525@pjwstk.edu.pl>
2023-07-05 22:40:47 +0200

    GET endpoint fix for titles

--- src/main/java/com/example/videozone/controller/VideoController.java
+++ src/main/java/com/example/videozone/controller/VideoController.java
@@ -1,13 +1,15 @@
 package com.example.videozone.controller;
 
+import com.example.videozone.model.Video;
 import com.example.videozone.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/videos")
@@ -23,4 +25,11 @@ public class VideoController {
 
         return ResponseEntity.ok("Plik został pomyślnie przesłany.");
     }
+
+    @GetMapping("/titles")
+    public ResponseEntity<List<String>> getAllVideoTitles() {
+        List<String> titles = videoService.getAllVideoTitles();
+        return ResponseEntity.ok(titles);
+    }
+
 }
--- src/main/java/com/example/videozone/service/VideoService.java
+++ src/main/java/com/example/videozone/service/VideoService.java
@@ -1,19 +1,42 @@
 package com.example.videozone.service;
 
+import com.example.videozone.model.Video;
+import com.example.videozone.repository.VideoRepository;
+import com.mongodb.client.gridfs.GridFSFindIterable;
+import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import com.mongodb.client.gridfs.model.GridFSFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.mongodb.core.query.Query;
 
 @Service
 public class VideoService {
+    @Autowired
+    private GridFsOperations gridFsOperations;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Autowired
     private GridFsTemplate gridFsTemplate;
+
+    private final VideoRepository videoRepository;
+
+    public VideoService(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
 
     @Transactional
     public void uploadVideo(MultipartFile file) {
@@ -27,5 +50,49 @@ public class VideoService {
             // Obsługa innych wyjątków
             throw new RuntimeException("Wystąpił nieznany błąd.", e);
         }
+    }
+
+
+    public List<Video> getAllVideos() {
+        List<Video> videos = videoRepository.findAll();
+
+        List<String> videoIds = new ArrayList<>();
+        for (Video video : videos) {
+            videoIds.add(video.getId());
+        }
+
+        List<String> titles = new ArrayList<>();
+        for (String videoId : videoIds) {
+            Query query = Query.query(Criteria.where("_id").is(videoId));
+            GridFSFile gridFSFile = gridFsTemplate.findOne(query);
+            if (gridFSFile != null) {
+                titles.add(gridFSFile.getMetadata().getString("title"));
+            }
+        }
+
+        // Do whatever you want with the titles...
+
+        return videos;
+    }
+
+    public List<String> getAllVideoTitles() {
+        Query query = new Query();
+        GridFSFindIterable files = gridFsTemplate.find(query);
+
+        List<String> titles = new ArrayList<>();
+        for (GridFSFile file : files) {
+            Document metadata = file.getMetadata();
+            if (metadata != null) {
+                if (metadata.containsKey("title")) {
+                    titles.add(metadata.getString("title"));
+                } else {
+                    titles.add(file.getFilename());
+                }
+            } else {
+                titles.add(file.getFilename());
+            }
+        }
+
+        return titles;
     }
 }
