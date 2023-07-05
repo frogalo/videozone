617cff3627f277f55bff4767d6ce9121f3fb4b1f
Jakub Urbanski <s23525@pjwstk.edu.pl>
2023-07-05 20:42:20 +0200

    init

---
+++ src/main/java/com/example/videozone/VideoZoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.videozone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoZoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/videozone/VideoZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videozone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
